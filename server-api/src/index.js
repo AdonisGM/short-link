1c2f002e12473432a9a1834c79456da1aa49cfcc
Nguyen Manh Tung <study@nmtung.dev>
2022-08-03 00:40:00 +0700

    nit project

---
+++ server-api/src/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.use()
+app.listen(3000, () => {
+  console.log(`-= server started =-`)
+})
---
+++ server-api/src/models/user.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+
+const {Schema} = mongoose
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    minLength: 1,
+    maxLength: 200,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    minLength: 1,
+    maxLength: 200,
+    index: true,
+    unique: true,
+    lowercase: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  accountType: {
+    type: String,
+    enum: ['basic', 'vip', 'premium'],
+    default: 'basic',
+  },
+}, {timestamps: true})
+
+module.exports = mongoose.model('user', userSchema)
